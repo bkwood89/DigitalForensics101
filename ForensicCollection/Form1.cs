9bef954b18e8f81c9bc2022c85ddb3b0dd7bf470
wesleywh <wesleyhaws1@gmail.com>
2015-10-25 14:25:16 -0600

    Initial Commit

---
+++ ForensicCollection/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection; //for assembly
+using System.Diagnostics; //for process
+using System.IO; //for file access
+using System.Security.Cryptography; //for sha1sum of files
+using System.Net; // for downloading web files
+
+namespace ForensicCollection
+{
+    public partial class Form1 : Form
+    {
+        private string errorOnTools;
+        private string toolsRoot = Directory.GetCurrentDirectory() + "/RawTools/";
+        private string saveOption;
+        WebClient webClient;               // Our WebClient that will be doing the downloading for us
+        Stopwatch sw = new Stopwatch();    // The stopwatch which we will be using to calculate the download speed
+        public object FPort { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string netcat = "" + portTextBox.Text;
+            output.Text = "";
+            warningText.Text = "";
+            if (String.IsNullOrEmpty(saveOption) == true)
+            {
+                warningText.Text = "Warning: You haven't Selected A Save Option Yet";
+                return;
+            }
+            if (fport.Checked == true)
+            {
+                saveOutput(runCommand("Fport", toolsRoot), "fport.txt");
+            }
+            if (ntlast.Checked == true)
+            {
+                saveOutput(runCommand("NTLast", toolsRoot), "NTLast.txt");
+            }
+            if (userdump.Checked == true)
+            {
+                saveOutput(runCommand("userdump -p", toolsRoot + "/OEM/userdump/"), "userdump.txt");
+            }
+            if (netstat_an.Checked == true)
+            {
+                saveOutput(runCommand("netstat -an", toolsRoot), "netstat-an.txt");
+            }
+            if (netstat_rn.Checked == true)
+            {
+                saveOutput(runCommand("netstat -rn", toolsRoot), "netstat-rn.txt");
+            }
+            if (psfile.Checked == true)
+            {
+                saveOutput(runCommand("psfile", toolsRoot + "/SysInternals/"), "psfile.txt");
+            }
+            if (pslist.Checked == true)
+            {
+                saveOutput(runCommand("pslist", toolsRoot + "/SysInternals/"), "pslist.txt");
+            }
+            if (ipconfig_all.Checked == true)
+            {
+                saveOutput(runCommand("ipconfig -all", toolsRoot), "ipconfig-all.txt");
+            }
+            if (date.Checked == true)
+            {
+                saveOutput(runCommand("date", toolsRoot), "date.txt");
+            }
+            if (time.Checked == true)
+            {
+                saveOutput(runCommand("time", toolsRoot), "time.txt");
+            }
+            if (psinfo_h_s_d.Checked == true)
+            {
+                saveOutput(runCommand("psinfo -h -s -d", toolsRoot + "/SysInternals/"), "psinfo-h-s-d.txt");
+            }
+            if (psloggedon.Checked == true)
+            {
+                saveOutput(runCommand("psloggedon", toolsRoot + "/SysInternals/"), "psloggedon.txt");
+            }
+            if (psservice.Checked == true)
+            {
+                saveOutput(runCommand("psservice", toolsRoot + "/SysInternals/"), "psservice.txt");
+            }
+            if (schtasks.Checked == true)
+            {
+                saveOutput(runCommand("schtasks", toolsRoot + "/SysInternals/"), "schtasks.txt");
+            }
+            if (find.Checked == true)
+            {
+                saveOutput(runCommand("find c:/ -printf '%m;%Ax;%AT;%Tx;%TT;%Cx;%CT;%U;%G;%s;%p\n'", toolsRoot + "/UnixUtils/usr/local/wbin/"), "find.txt");
+            }
+            if (psloglist.Checked == true)
+            {
+                saveOutput(runCommand("psloglist -s -x security", toolsRoot + "/SysInternals/"), "psloglist.txt");
+            }
+            if (sha1sum.Checked == true)
+            {
+                sha1sumAllFiles(Directory.GetCurrentDirectory() + "/Evidence/");
+            }
+            output.Text += "Finished Running All Commands.";
+            if (string.IsNullOrEmpty(errorOnTools) == false)
+            {
+                MessageBox.Show("There were errors while trying to run the following commands:\n" + errorOnTools + "\nto prevent these simply setup your forensic enviornment by clicking the 'Get Tools' button and then pressing 'Start'.");
+            }
+       }
+
+        private string runCommand(string commandToExecute, string dirToExecute)
+        {
+            output.Text += "Running " + commandToExecute + "...";
+            string commandOutput = string.Empty;//clear any possible residual strings
+            string error = string.Empty;//clear any possible residual strings
+
+            // ProcessStartInfo navigate = new ProcessStartInfo("cmd", "cd " + Directory.GetCurrentDirectory() + "/RawTools/");
+            // Process.Start(navigate);
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd", "/c " + commandToExecute);
+            processStartInfo.WorkingDirectory = dirToExecute;
+            processStartInfo.RedirectStandardOutput = true;//allows use to redirect ouput (normal)
+            processStartInfo.RedirectStandardError = true;//allows use to redirect ouput (errors)
+            processStartInfo.CreateNoWindow = true;
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;//don't show cmd prompt
+            processStartInfo.UseShellExecute = false;//this is to allow the redirecting of output to our string
+
+            Process process = Process.Start("cmd", "cd " + Directory.GetCurrentDirectory() + "/RawTools/");
+            try {
+                process = Process.Start(processStartInfo);
+            }
+            catch(Exception e)
+            {
+                errorOnTools += commandToExecute+"\n";
+            }
+            using (StreamReader streamReader = process.StandardOutput)
+            {
+                commandOutput = streamReader.ReadToEnd();//capture normal output
+            }
+
+            using (StreamReader streamReader = process.StandardError)
+            {
+                error = streamReader.ReadToEnd();//capture any errors
+                output.Text += "\n" + error + "... ";
+            }
+            //Consolidate Errors and output into one long line
+            string finalOutput = commandOutput + " " + error;
+            output.Text += "Done.\n";
+            return finalOutput;
+        }
+        private void saveOutput(string textForFile, string fileName)
+        {
+            output.Text += "Saving " + fileName + "...";
+            //make folder at exe location and save txt
+            string path = Directory.GetCurrentDirectory() + "/Evidence/" + fileName;
+            //If folder doesn't exists create it otherwise just access it and save to it
+            bool folderExists = Directory.Exists(Directory.GetCurrentDirectory() + "/Evidence/");
+            if (!folderExists)
+            {
+                output.Text += "Creating Folder...";
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/Evidence/");
+            }
+            //Create the txt file at path
+            if (!File.Exists(path))
+            {
+                var outputFile = File.Create(path);
+                outputFile.Close();
+            }
+            //write the output to the text file
+            File.WriteAllText(path, textForFile);
+            output.Text += "Done.\n";
+        }
+        private void sha1sumAllFiles(string directory)
+        {
+            output.Text += "Taking sha1sum's of all files...\n";
+            // Create a DirectoryInfo object representing the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(directory);
+            // Get the FileInfo objects for every file in the directory.
+            FileInfo[] files = dir.GetFiles();
+            // Initialize a RIPE160 hash object.
+            RIPEMD160 myRIPEMD160 = RIPEMD160Managed.Create();
+            byte[] hashValue;
+            // Compute and print the hash values for each file in directory.
+            foreach (FileInfo fInfo in files)
+            {
+                output.Text += "Taking sha1sum for " + fInfo.Name + "...";
+                // Create a fileStream for the file.
+                FileStream fileStream = fInfo.Open(FileMode.Open);
+                // Be sure it's positioned to the beginning of the stream.
+                fileStream.Position = 0;
+                // Compute the hash of the fileStream.
+                hashValue = myRIPEMD160.ComputeHash(fileStream);
+                //conver the byte array to a HEX string
+                string convertedHash = HexStringFromBytes(hashValue);
+                // Write the Hex value to a file called <FILENAME>.sha
+                if (File.Exists(Directory.GetCurrentDirectory() + "/Evidence/" + Path.GetFileNameWithoutExtension(fInfo.Name) + ".sha"))
+                {
+                    warningText.Text = "Warning: Ended with errors. Check the output window for information.";
+                    string errorText = "\nERROR:File already exsists. Remove it and try again.\n";
+                    output.AppendText(errorText);
+                }
+                else
+                {
+                    File.WriteAllText(directory + "/" + Path.GetFileNameWithoutExtension(fInfo.Name) + ".sha", convertedHash + " *" + Path.GetFileName(fInfo.Name));
+                }
+                // Close the file.
+                fileStream.Close();
+                output.Text += "Done.\n";
+            }
+            output.Text += "Finished taking sha1sums of all files.\n";
+        }
+
+        public static string HexStringFromBytes(byte[] bytes)
+        {
+            var sb = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                var hex = b.ToString("x2");
+                sb.Append(hex);
+            }
+            return sb.ToString();
+        }
+        private void saveBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Reset Everything on every call
+            portText.Visible = false;
+            portTextBox.Visible = false;
+            warningText.Text = "";
+            //decide what has been selected
+            saveOption = saveBox.SelectedItem.ToString();
+            //logic for selected options
+            if (saveOption == "Netcat Session")
+            {
+                portText.Text = "Send Through Port:";
+                portText.Visible =  true;
+                portTextBox.Visible = true;
+            }
+            else if (saveOption == "Local")
+            {
+                portText.Text = "This will save everything in the 'Evidence' Folder. ";
+                portText.Visible = true;
+            }
+        }
+
+        private void toolsButton_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+            form2 = null;
+            Show();
+        }
+    }
+}
---
+++ ForensicCollection/Form2.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net; // for downloading web files
+using System.IO; //for file access
+using System.IO.Compression; //for extracting zip files
+using System.Diagnostics; //for process
+
+namespace ForensicCollection
+{
+    public partial class Form2 : Form
+    {
+        WebClient webClient;               // Our WebClient that will be doing the downloading for us
+        Stopwatch sw = new Stopwatch();    // The stopwatch which we will be using to calculate the download speed
+        string downloadFolder = Directory.GetCurrentDirectory() + "/downloadedFiles";
+        string rawToolsDirectory = Directory.GetCurrentDirectory() + "/RawTools";
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            //toolsListText.Visible = false;
+            //fport.Visible = false;
+            //ntlast.Visible = false;
+            //sysinternals.Visible = false;
+            //oem.Visible = false;
+            //unixutils.Visible = false;
+            warningText.Visible = false;
+
+            //labelSpeed.Visible = true;
+            //labelPerc.Visible = true;
+            //labelDownloaded.Visible = true;
+            //progressBar.Visible = true;
+            filename.Visible = true;
+
+            //labelSpeed.Refresh();
+            //labelPerc.Refresh();
+            //labelDownloaded.Refresh();
+            //progressBar.Refresh();
+            filename.Refresh();
+
+            if (FileExistCheck() == true)
+            {
+                //labelSpeed.Visible = false;
+                //labelPerc.Visible = false;
+                //labelDownloaded.Visible = false;
+                //progressBar.Visible = false;
+                filename.Visible = false;
+            }
+            else
+            {
+                phase1.Visible = true;
+                phase1.Refresh();
+                DownloadAllFiles();
+
+                phase2.Visible = true;
+                phase2.Refresh();
+                ExtractAllFiles();
+
+                phase3.Visible = true;
+                phase3.Refresh();
+                MoveAllFiles();
+                
+                if (keepFiles.Checked == false)
+                {
+                    Directory.Delete(downloadFolder,true);
+                }
+                else
+                {
+                    cleanDownloadsFolder();
+                }
+                phasecomplete.Visible = true;
+                finishText.Visible = true;
+            }
+        }
+        private bool FileExistCheck()
+        {
+            if (Directory.Exists(rawToolsDirectory + "/OEM"))
+            {
+                MessageBox.Show("Please clear all files From your 'RawTools' directory to continue.");
+                return true;
+            }
+            if (Directory.Exists(rawToolsDirectory + "/SysInternals"))
+            {
+                MessageBox.Show("Please clear all files From your 'RawTools' directory to continue.");
+                return true;
+            }
+            if (Directory.Exists(rawToolsDirectory + "/UnixUtils"))
+            {
+                MessageBox.Show("Please clear all files From your 'RawTools' directory to continue.");
+                return true;
+            }
+            if (File.Exists(rawToolsDirectory + "/Fport.exe"))
+            {
+                MessageBox.Show("Please clear all files From your 'RawTools' directory to continue.");
+                return true;
+            }
+            if (File.Exists(rawToolsDirectory + "/NTLast.exe"))
+            {
+                MessageBox.Show("Please clear all files From your 'RawTools' directory to continue.");
+                return true;
+            }
+            return false;
+        }
+        private void DownloadAllFiles()
+        {
+            bool downloadFolderExists = Directory.Exists(downloadFolder);
+            if (downloadFolderExists == false)
+            {
+                Directory.CreateDirectory(downloadFolder);
+            }
+            if (File.Exists(downloadFolder + "/fport.zip") == false)
+            {
+                filename.Text = "Downloading Fport v2.0 ...";
+                filename.Refresh();
+                fport.Text += "...Downloading.";
+                fport.Refresh();
+                DownloadFile("http://b2b-download.mcafee.com/products/tools/foundstone/fport.zip", downloadFolder + "/fport.zip");
+                fport.Text += "Done.";
+                fport.Refresh();
+            }
+            if (File.Exists(Directory.GetCurrentDirectory() + "/downloadedFile/ntlast30.zip") == false)
+            {
+                filename.Text = "Downloading NTLast v3.0...";
+                filename.Refresh();
+                ntlast.Text += "...Downloading.";
+                ntlast.Refresh();
+                DownloadFile("http://b2b-download.mcafee.com/products/tools/foundstone/ntlast30.zip", downloadFolder + "/ntlast30.zip");
+                ntlast.Text += "Done.";
+                ntlast.Refresh();
+            }
+            if (File.Exists(Directory.GetCurrentDirectory() + "/downloadedFile/oem3sr2.zip") == false)
+            {
+                filename.Text = "Downloading Microsoft's OEM Support Tools...";
+                filename.Refresh();
+                oem.Text += "...Downloading.";
+                oem.Refresh();
+                DownloadFile("http://download.microsoft.com/download/win2000srv/utility/3.0/nt45/en-us/oem3sr2.zip", downloadFolder + "/oem3sr2.zip");
+                oem.Text += "Done.";
+                oem.Refresh();
+            }
+            if (File.Exists(Directory.GetCurrentDirectory() + "/downloadedFile/UnxUtils.zip") == false)
+            {
+                filename.Text = "Downloading Unix Utilities...";
+                filename.Refresh();
+                unixutils.Text += "...Downloading.";
+                unixutils.Refresh();
+                DownloadFile("http://superb-dca2.dl.sourceforge.net/project/unxutils/unxutils/current/UnxUtils.zip", downloadFolder + "/UnxUtils.zip");
+                unixutils.Text += "Done.";
+                unixutils.Refresh();
+            }
+            if (File.Exists(Directory.GetCurrentDirectory() + "/downloadedFile/SysinternalsSuite.zip") == false)
+            {
+                filename.Text = "Downloading Microsoft's System Internals Suite...";
+                filename.Refresh();
+                sysinternals.Text += "Downloading.";
+                sysinternals.Refresh();
+                DownloadFile("https://download.sysinternals.com/files/SysinternalsSuite.zip", downloadFolder + "/SysinternalsSuite.zip");
+                sysinternals.Text += "Done.";
+                sysinternals.Refresh();
+            }
+            sw.Stop();
+        }
+        private void ExtractAllFiles()
+        {
+            bool rawToolsFolderExists = Directory.Exists(rawToolsDirectory);
+            bool oemFolderExists = Directory.Exists(rawToolsDirectory + "/OEM");
+            bool sysInternalsFolderExists = Directory.Exists(rawToolsDirectory + "/SysInternals");
+            bool unixUtilsFolderExists = Directory.Exists(rawToolsDirectory + "/UnixUtils");
+
+            if (rawToolsFolderExists == false)
+            {
+                Directory.CreateDirectory(rawToolsDirectory);
+            }
+            if (oemFolderExists == false)
+            {
+                Directory.CreateDirectory(rawToolsDirectory + "/OEM");
+            }
+            if (sysInternalsFolderExists == false)
+            {
+                Directory.CreateDirectory(rawToolsDirectory + "/SysInternals");
+            }
+            if (unixUtilsFolderExists == false)
+            {
+                Directory.CreateDirectory(rawToolsDirectory + "/UnixUtils");
+            }
+            filename.Text = "Extracting Files and Setting Up Enviornment...";
+            fport.Text += "..Extracting";
+            fport.Refresh();
+            ZipFile.ExtractToDirectory(downloadFolder + "/fport.zip", downloadFolder+"/");
+
+            ntlast.Text = "Fport..Extracting";
+            ntlast.Refresh();
+            ZipFile.ExtractToDirectory(downloadFolder + "/ntlast30.zip", downloadFolder+"/");
+
+            oem.Text = "Microsofts OEM Support Tools..Extracting";
+            oem.Refresh();
+            ZipFile.ExtractToDirectory(downloadFolder + "/oem3sr2.zip", rawToolsDirectory + "/OEM/");
+
+            sysinternals.Text = "Microsofts System Internals..Extracting";
+            sysinternals.Refresh();
+            ZipFile.ExtractToDirectory(downloadFolder + "/SysinternalsSuite.zip", rawToolsDirectory + "/SysInternals/");
+
+            unixutils.Text = "Unix Utilities..Extracting";
+            unixutils.Refresh();
+            ZipFile.ExtractToDirectory(downloadFolder + "/UnxUtils.zip", rawToolsDirectory + "/UnixUtils/");
+        }
+        private void MoveAllFiles()
+        {
+            filename.Text = "Moving Extracted Files...";
+            filename.Refresh();
+            fport.Text += "..Moving.";
+            fport.Refresh();
+            try {
+                if(File.Exists(rawToolsDirectory + "/Fport.exe"))
+                {
+                    File.Delete(rawToolsDirectory + "/Fport.exe");
+                }
+                File.Move(downloadFolder + "/Fport-2.0/Fport.exe", rawToolsDirectory + "/Fport.exe");
+                Directory.Delete(downloadFolder + "/Fport-2.0/", true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            File.Delete(downloadFolder + "/fport.zip");
+            fport.Text += "Done.";
+            fport.Refresh();
+
+            ntlast.Text += "..Moving.";
+            try {
+                if (File.Exists(rawToolsDirectory + "/NTLast.exe"))
+                {
+                    File.Delete(rawToolsDirectory + "/NTLast.exe");
+                }
+                //File.Move(downloadFolder + "/Fport-2.0/Fport.exe", rawToolsDirectory + "/Fport.exe");
+                File.Move(downloadFolder + "/NTLast.exe", rawToolsDirectory + "/NTLast.exe");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            ntlast.Text += "Done.";
+            ntlast.Refresh();
+
+            oem.Text += "..Moving";
+            oem.Refresh();
+            //try {
+            //    List<String> oemFiles = Directory.GetFiles(downloadFolder + "/OEM/", "*.*", SearchOption.AllDirectories).ToList();
+            //    foreach (string file in oemFiles)
+            //    {
+            //        FileInfo mFile = new FileInfo(file);
+            //        mFile.MoveTo(rawToolsDirectory + "/OEM/" + mFile.Name);
+            //    }
+            //}
+            //catch(Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+            oem.Text += "Done.";
+            oem.Refresh();
+
+            sysinternals.Text += "..Moving.";
+            sysinternals.Refresh();
+            //try {
+            //    List<String> sysInternalFiles = Directory.GetFiles(downloadFolder + "/SysInternals/", "*.*", SearchOption.AllDirectories).ToList();
+            //    foreach (string file in sysInternalFiles)
+            //    {
+            //        FileInfo mFile = new FileInfo(file);
+            //        mFile.MoveTo(rawToolsDirectory + "/SysInternals/" + mFile.Name);
+            //    }
+            //}
+            //catch(Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+            sysinternals.Text += "Done.";
+            sysinternals.Refresh();
+
+            unixutils.Text += "..Moving.";
+            unixutils.Refresh();
+            //try {
+            //    List<String> unixUtilesFiles = Directory.GetFiles(downloadFolder + "/UnixUtils/", "*.*", SearchOption.AllDirectories).ToList();
+            //    foreach (string file in unixUtilesFiles)
+            //    {
+            //        FileInfo mFile = new FileInfo(file);
+            //        mFile.MoveTo(rawToolsDirectory + "/UnixUtils/" + mFile.Name);
+            //    }
+            //}
+            //catch(Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+            unixutils.Text += "Done.";
+            unixutils.Refresh();
+        }
+        private void cleanDownloadsFolder()
+        {
+            DirectoryInfo di = new DirectoryInfo(downloadFolder);
+            FileInfo[] files = di.GetFiles("*.txt")
+                                 .Where(p => p.Extension == ".txt").ToArray();
+            foreach (FileInfo file in files)
+                try
+                {
+                    file.Attributes = FileAttributes.Normal;
+                    File.Delete(file.FullName);
+                }
+                catch (Exception ex){
+                    MessageBox.Show(ex.Message);
+                }
+        }
+        private void DownloadFile(string urlAddress, string saveLocation)
+        {
+            using (webClient = new WebClient())
+            {
+                sw.Start();//start the download speed timer
+                //webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                //webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                // The variable that will be holding the url address (making sure it starts with http://)
+                Uri URL = new Uri(urlAddress);//urlAddress.StartsWith("http://", StringComparison.OrdinalIgnoreCase) ? new Uri(urlAddress) : new Uri("http://" + urlAddress);
+                try
+                {
+                    // Start downloading the file
+                    webClient.DownloadFile(URL, saveLocation);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        // The event that will fire whenever the progress of the WebClient is changed
+        //private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        //{
+        //    // Calculate download speed and output it to labelSpeed.
+        //    labelSpeed.Text = string.Format("{0} kb/s", (e.BytesReceived / 1024d / sw.Elapsed.TotalSeconds).ToString("0.00"));
+        //    // Update the progressbar percentage only when the value is not the same.
+        //    progressBar.Value = e.ProgressPercentage;
+        //    // Show the percentage on our label.
+        //    labelPerc.Text = e.ProgressPercentage.ToString() + "%";
+        //    // Update the label with how much data have been downloaded so far and the total size of the file we are currently downloading
+        //    labelDownloaded.Text = string.Format("{0} MB's / {1} MB's",
+        //        (e.BytesReceived / 1024d / 1024d).ToString("0.00"),
+        //        (e.TotalBytesToReceive / 1024d / 1024d).ToString("0.00"));
+        //}
+
+        // The event that will trigger when the WebClient is completed
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            // Reset the stopwatch.
+            sw.Reset();
+
+            if (e.Cancelled == true)
+            {
+                MessageBox.Show("Download has been canceled.");
+            }
+            else
+            {
+                MessageBox.Show("Download completed!");
+            }
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+    }
+}
